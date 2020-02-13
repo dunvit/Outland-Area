63b5a1f1520e25f18d072c6fb4d6e534c2dc8f73
dunvit <sushilov.vitaly@gmail.com>
2020-02-13 12:16:31 +0200

    Add unit tests for basic characters system

--- Tests/CharacterSystem/CharacterTests.cs
+++ Tests/CharacterSystem/CharacterTests.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OutlandAreaLogic;
 using OutlandAreaLogic.CharacterSystem;
 
 namespace Tests.CharacterSystem
 {
-    [TestClass()]
+    [TestClass]
     public class CharacterTests
     {
-        [TestMethod()]
+        [TestMethod]
         public void CharacterTest()
         {
             var character = new Character(637066561468099897);
+
+            Assert.AreEqual("Joanna Benbow", character.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Critical error on read character data from file.")]
+        public void GetCharacterNegativeTest()
+        {
+            var character = Global.Characters.GetCharacter(637066561468099891);
+        }
+
+        [TestMethod()]
+        public void GetCharacterPositiveTest()
+        {
+            var character = Global.Characters.GetCharacter(637066561468099897);
 
             Assert.AreEqual("Joanna Benbow", character.Name);
         }
