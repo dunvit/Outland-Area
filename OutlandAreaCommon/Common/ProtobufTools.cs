42b0f9988b1c10582fa8492c617e816e4014aa3a
Sushilov Vitaly <sushilov.vitaly@gmail.com>
2020-12-07 09:08:57 +0200

    Add protobuf tools and nuget

---
+++ OutlandAreaCommon/Common/ProtobufTools.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using ProtoBuf;
+
+namespace OutlandAreaCommon.Common
+{
+    /// <summary>
+    /// Using:
+    /// Class or collection of ProtoContract objects
+    /// var dataAfterSerialize      = ProtobufTools.Serialize(data);
+    /// var stringAfterDeserialize  = ProtobufTools.Deserialize<List<Pilot>>(dataAfterSerialize);
+    /// </summary>
+
+    public class ProtobufTools
+    {
+        public static byte[] Serialize<T>(T record) where T : class
+        {
+            if (null == record) return null;
+
+            try
+            {
+                using var stream = new MemoryStream();
+                Serializer.Serialize(stream, record);
+
+                return stream.ToArray();
+            }
+            catch
+            {
+                // Log error
+                throw;
+            }
+
+
+        }
+
+        public static T Deserialize<T>(byte[] data) where T : class
+        {
+            if (null == data) return null;
+
+            try
+            {
+                using var stream = new MemoryStream(data);
+
+                return Serializer.Deserialize<T>(stream);
+            }
+            catch
+            {
+                // Log error
+                throw;
+            }
+        }
+    }
+}
