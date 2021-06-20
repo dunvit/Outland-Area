91c087b4162103ea7553b12f19897e8235469ce0
Vitaly Sushilov <vitaly.sushilov@contentsquare.com>
2021-06-20 17:30:47 +0300

    Outer Space unit tests

---
+++ Outland Area CoreTests/Tools/OuterSpaceTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Outland_Area_CoreTests;
+
+namespace Engine.Tools.Tests
+{
+    [TestClass()]
+    public class OuterSpaceTests
+    {
+        [TestMethod()]
+        public void MouseClickOnObjectAndChangeSelectedObject_Test()
+        {
+            var receivedEvents = new List<int>();
+
+            var server = EnvironmentGlobal.CreateGameServer("OuterSpace");
+
+            var gameSession = server.RefreshGameSession(server.SessionId);
+
+            var outerSpace = new OuterSpace();
+
+            var isChangeSelectedObject = false;
+
+            outerSpace.OnChangeSelectedObject += (e) =>
+            {
+                Assert.AreEqual(e, 5006);
+                isChangeSelectedObject = true;
+            };
+
+            outerSpace.Refresh(gameSession, new System.Drawing.PointF(10101, 10201), 1);
+
+            Assert.IsTrue(isChangeSelectedObject);
+
+        }
+
+        [TestMethod()]
+        public void MouseClickOnObjectAndChangeActiveObject_Test()
+        {
+            var receivedEvents = new List<int>();
+
+            var server = EnvironmentGlobal.CreateGameServer("OuterSpace");
+
+            var gameSession = server.RefreshGameSession(server.SessionId);
+
+            var outerSpace = new OuterSpace();
+
+            var isChangeActiveObject = false;
+
+            outerSpace.OnChangeActiveObject += (e) =>
+            {
+                Assert.AreEqual(e, 5006);
+                isChangeActiveObject = true;
+            };
+
+            outerSpace.Refresh(gameSession, new System.Drawing.PointF(10101, 10201), 2);
+
+            Assert.IsTrue(isChangeActiveObject);
+
+        }
+
+        [TestMethod()]
+        public void MouseClickOnObjectAndChangeSelectedObject_Negative_Test()
+        {
+            var receivedEvents = new List<int>();
+
+            var server = EnvironmentGlobal.CreateGameServer("OuterSpace");
+
+            var gameSession = server.RefreshGameSession(server.SessionId);
+
+            var outerSpace = new OuterSpace();
+
+            var isChangeSelectedObject = false;
+
+            outerSpace.OnChangeSelectedObject += (e) =>
+            {
+                Assert.AreEqual(e, 5006);
+                isChangeSelectedObject = true;
+            };
+
+            outerSpace.Refresh(gameSession, new System.Drawing.PointF(9101, 9201), 1);
+
+            Assert.IsFalse(isChangeSelectedObject);
+
+        }
+
+        [TestMethod()]
+        public void MouseClickOnObjectAndChangeActiveObject_Negative_Test()
+        {
+            var receivedEvents = new List<int>();
+
+            var server = EnvironmentGlobal.CreateGameServer("OuterSpace");
+
+            var gameSession = server.RefreshGameSession(server.SessionId);
+
+            var outerSpace = new OuterSpace();
+
+            var isChangeActiveObject = false;
+
+            outerSpace.OnChangeActiveObject += (e) =>
+            {
+                if( e > 0)
+                    isChangeActiveObject = true;
+            };
+
+            outerSpace.Refresh(gameSession, new System.Drawing.PointF(9101, 9201), 2);
+
+            Assert.IsFalse(isChangeActiveObject);
+
+        }
+    }
+}
--- Outland Area/Tools/OuterSpace.cs
+++ Outland Area/Tools/OuterSpace.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using EngineCore.DataProcessing;
 using EngineCore.Session;
-using EngineCore.Tools;
 using EngineCore.Universe.Model;
 using EngineCore.Universe.Objects;
 
