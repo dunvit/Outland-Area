9e7dc14b78fffa31494b88f6c382fecebaaf1d01
Sushilov Vitaly <sushilov.vitaly@gmail.com>
2021-06-28 14:23:33 +0300

    1

---
+++ Outland Area CoreTests/DAL/GameSessionTurnsTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Outland_Area_CoreTests.DAL
+{
+    [TestClass]
+    public class GameSessionTurnsTests
+    {
+        [TestMethod()]
+        public void TurnRecalculation_Test()
+        {
+            var server = EnvironmentGlobal.CreateGameServer("General");
+
+            server.TurnCalculation(1);
+
+            var gameSession = server.RefreshGameSession(server.SessionId);
+
+            Assert.AreEqual(1, gameSession.Turn);
+
+            server.TurnCalculation(5);
+
+            gameSession = server.RefreshGameSession(server.SessionId);
+
+            Assert.AreEqual(6, gameSession.Turn);
+        }
+
+        [TestMethod()]
+        public void SessionId_Test()
+        {
+            var server = EnvironmentGlobal.CreateGameServer("General");
+
+            server.TurnCalculation(1);
+
+            var gameSession = server.RefreshGameSession(server.SessionId);
+
+            Assert.AreEqual(1005302115, gameSession.Id);
+
+            server.TurnCalculation(5);
+
+            gameSession = server.RefreshGameSession(server.SessionId);
+
+            Assert.AreEqual(1005302115, gameSession.Id);
+
+            server = EnvironmentGlobal.CreateGameServer("Reloading");
+
+            server.TurnCalculation(1);
+
+            gameSession = server.RefreshGameSession(server.SessionId);
+
+            Assert.AreEqual(1005302116, gameSession.Id);
+
+            server.TurnCalculation(5);
+
+            gameSession = server.RefreshGameSession(server.SessionId);
+
+            Assert.AreEqual(1005302116, gameSession.Id);
+        }
+    }
+}
